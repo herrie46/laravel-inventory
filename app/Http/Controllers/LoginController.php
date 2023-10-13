db7d70653a5379f7a469474cd56f90c24e48f3fc
herrie46 <herrieaps@gmail.com>
2023-10-13 19:14:02 +0700

    Upload Library font awesome

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// Jadi, ketika Anda menulis "use Illuminate\Http\Request"
+// , Anda sedang memberi tahu PHP bahwa Anda ingin menggunakan kelas Request
+// yang ada di dalam ruang nama Illuminate\Http. Dengan melakukan ini
+// , Anda dapat membuat objek dari kelas Request dan menggunakannya untuk
+// mengakses informasi tentang permintaan HTTP yang saat ini
+// sedang diolah oleh aplikasi Laravel Anda.
 use Illuminate\Http\RedirectResponse;
+// Illuminate\Http\RedirectResponse" adalah kelas yang digunakan dalam Laravel
+// untuk membuat respons yang mengarahkan (redirect) pengguna ke halaman atau
+// URL lain setelah suatu tindakan selesai
 use Illuminate\Support\Facades\Auth;
+// Dengan menggunakan "use Illuminate\Support\Facades\Auth," Anda dapat dengan mudah
+// mengakses bagian depan Auth dan menggunakan berbagai metode otentikasi yang disediakan
+// oleh Laravel untuk mengelola otentikasi pengguna dalam aplikasi Anda
 
 class LoginController extends Controller
 {
@@ -25,4 +37,8 @@ class LoginController extends Controller
             'email' => 'The provided credentials do not match our records.',
         ])->onlyInput('email');
     }
+    // Dengan demikian, metode authenticate ini berfungsi untuk memvalidasi
+    // kredensial pengguna, mengautentikasi mereka, dan mengarahkan mereka ke
+    // halaman yang sesuai tergantung pada hasil autentikasi. Jika autentikasi gagal
+    // , pesan kesalahan akan ditampilkan kepada pengguna.
 }
