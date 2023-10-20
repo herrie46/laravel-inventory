3973c089ac310107c49ca4a9d10d70580a0604ae
herrie46 <herrieaps@gmail.com>
2023-10-20 21:04:49 +0700

    menyamnbungkan file view dengan template sbadmin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,7 @@
+@extends('template/index')
+@section('konten')
+{{-- akan masuk ke yield mana --}}
 <h1>Halaman Dashboard</h1>
-<a href="{{route('logout')}}">
-    <button>Keluar</button>
-</a>
+@endsection
+
 
