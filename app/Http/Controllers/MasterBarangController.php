52667c56711670614bca207d254ee9f2c6259557
herrie46 <herrieaps@gmail.com>
2023-10-27 21:15:45 +0700

    Master Barang : proses simpan ke DB

--- app/Http/Controllers/MasterBarangController.php
+++ app/Http/Controllers/MasterBarangController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\MasterBarangModel;
+use Illuminate\Support\Facades\Auth;
+
 
 class MasterBarangController extends Controller
 {
@@ -26,8 +28,31 @@ class MasterBarangController extends Controller
 
     public function store(Request $request)
     {
-        //
+    //   dd($request)
+    try{
+      $insert = MasterBarangModel::create([
+         'kode'         => $request->html_kode,
+         'nama'         => $request->html_nama,
+         'deskripsi'    => $request->html_deskripsi,
+         'id_kategori'  => null,
+         'id_gudang'    => null,
+         'dibuat_kapan' => date('Y-m-d H:i:s'),
+         'dibuat_oleh'  => Auth::user()->id,
+        ]);
+      //jika proses input ke mysql berhasil
+      if($insert) {
+        return redirect()
+        -> route('master-barang')
+        -> with('Success','Berhasil menambahkan barang baru!');
+                  }
+       }
+       catch (\Throwable $th) {
+        return redirect()
+        -> route('master-barang-tambah')
+        -> with('error',$th->getMessage());
+       }
     }
+
 
 
     public function show(string $id)
--- resources/views/master/barang/form-tambah.blade.php
+++ resources/views/master/barang/form-tambah.blade.php
@@ -1,17 +1,18 @@
 @extends('master/all')
 @section('master-konten')
 <form action="{{route('master-barang-simpan')}}" method="post">
+    @csrf
     <div class="mb-3">
         <label for="html_kode" class="form-label">Kode</label>
-        <input type="text" class="form-control w-50" id="html_kode" placeholder="Kode Barang">
+        <input type="text" class="form-control w-50" id="html_kode" name="html_kode" placeholder="Kode Barang">
       </div>
       <div class="mb-3">
         <label for="html_nama" class="form-label">Nama</label>
-        <input type="text" class="form-control w-50" id="html_nama" placeholder="Kode Barang">
+        <input type="text" class="form-control w-50" id="html_nama" name="html_nama" placeholder="Nama Barang">
       </div>
       <div class="mb-3">
         <label for="html_deskripsi" class="form-label">Deskripsi</label>
-        <textarea class="form-control" id="html_deskripsi" rows="3"></textarea>
+        <textarea class="form-control" id="html_deskripsi" name="html_deskripsi" rows="3"></textarea>
       </div>
       <button type="submit" class="btn btn-primary">
         <i class="fa fa-solid fa-save me-2"></i>Simpan
