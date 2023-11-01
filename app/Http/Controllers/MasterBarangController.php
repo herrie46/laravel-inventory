2fca08fdce92e8888ca7f1bb5c8514dde4a98ec0
herrie46 <herrieaps@gmail.com>
2023-11-01 20:48:52 +0700

    Validasi store master barang

--- app/Http/Controllers/MasterBarangController.php
+++ app/Http/Controllers/MasterBarangController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\MasterBarangModel;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
 
 
 class MasterBarangController extends Controller
@@ -28,8 +30,24 @@ class MasterBarangController extends Controller
 
     public function store(Request $request)
     {
-    //   dd($request)
+    $aturan =[
+        'html_kode' => 'required|min:3|max:7|alpha_dash',
+        'html_nama' => 'required|min:10|max:25',
+        'html_deskripsi' => 'max:255',
+    ];
+    $pesan_indo = [
+        'required' =>'Wajib Diisi',
+        'min' =>'Minimal :min Karakter',
+        'max' =>'Maksimal :max Karakter',
+        'alpha_dash' =>'Wajib menggunakan huruf,angka dan underscore'
+    ];
+    $validator = Validator::make($request->all(),$aturan,$pesan_indo);
     try{
+        if($validator->fails()) {
+            return redirect()
+            -> route('master-barang-tambah')
+            -> withErrors($validator)->withInput();
+        } else {
       $insert = MasterBarangModel::create([
          'kode'         => $request->html_kode,
          'nama'         => $request->html_nama,
@@ -45,7 +63,8 @@ class MasterBarangController extends Controller
         -> route('master-barang')
         -> with('success','Berhasil menambahkan barang baru!');
                   }
-       }
+                }
+        }
        catch (\Throwable $th) {
         return redirect()
         -> route('master-barang-tambah')
--- resources/views/master/barang/form-tambah.blade.php
+++ resources/views/master/barang/form-tambah.blade.php
@@ -4,15 +4,24 @@
     @csrf
     <div class="mb-3">
         <label for="html_kode" class="form-label">Kode</label>
-        <input type="text" class="form-control w-50" id="html_kode" name="html_kode" placeholder="Kode Barang" autofocus>
-      </div>
+        <input type="text" class="form-control w-50" id="html_kode" name="html_kode" value="{{old('html_kode')}}" placeholder="Kode Barang" autofocus>
+        @if($errors->has('html_kode'))
+            <div class="badge text-bg-danger">{{$errors->first('html_kode')}} </div>
+        @endif
+        </div>
       <div class="mb-3">
         <label for="html_nama" class="form-label">Nama</label>
-        <input type="text" class="form-control w-50" id="html_nama" name="html_nama" placeholder="Nama Barang">
-      </div>
+        <input type="text" class="form-control w-50" id="html_nama" name="html_nama" value="{{old('html_nama')}}" placeholder="Nama Barang">
+        @if($errors->has('html_nama'))
+        <div class="badge text-bg-danger">{{$errors->first('html_nama')}} </div>
+    @endif
+    </div>
       <div class="mb-3">
         <label for="html_deskripsi" class="form-label">Deskripsi</label>
-        <textarea class="form-control" id="html_deskripsi" name="html_deskripsi" rows="3"></textarea>
+        <textarea class="form-control" id="html_deskripsi" name="html_deskripsi" rows="3">{{old('html_deskripsi')}}</textarea>
+        @if($errors->has('html_deskripsi'))
+        <div class="badge text-bg-danger">{{$errors->first('html_deskripsi')}} </div>
+    @endif
       </div>
       <button type="submit" class="btn btn-primary">
         <i class="fa fa-solid fa-save me-2"></i>Simpan
